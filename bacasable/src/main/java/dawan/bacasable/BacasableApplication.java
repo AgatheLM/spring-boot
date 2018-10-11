8df0b3fdb7eb59e0c904fd6af219c4cd1142efb0
Agathe LE MEUR <agathe_lm12@hotmail.com>
2018-10-11 10:05:55 +0200

    init projet

---
+++ bacasable/src/main/java/dawan/bacasable/BacasableApplication.java
@@ -0,0 +1,12 @@
+package dawan.bacasable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BacasableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BacasableApplication.class, args);
+	}
+}
